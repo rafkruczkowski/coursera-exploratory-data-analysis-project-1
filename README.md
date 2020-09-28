68a5bb58eebbc6fb986489999f98742885f8aceb
rafkruczkowski@github.com <kruczkowski@gmail.com>
2020-09-27 20:21:09 -0700

    2

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# coursera-exploratory-data-analysis-project-1
+# Project 1
+## Coursera Exploratory Data Analysis Course
+### Sept 2020
+
+Project is based on the following link:
+`https://www.coursera.org/learn/exploratory-data-analysis/peer/ylVFo/course-project-1`
+
+Data is avalible at:
+`https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip`
--- plot1.R
+++ plot1.R
@@ -1,9 +1,13 @@
+# https://github.com/rafkruczkowski/coursera-exploratory-data-analysis-project-1
+
+# import and subset the data to the dates in 2007
 dataFile <- "./data/household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-#str(subSetData)
+# Generate Plot based on the subset of data
 globalActivePower <- as.numeric(subSetData$Global_active_power)
 png("plot1.png", width=480, height=480)
+# Generate a Histogram
 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
